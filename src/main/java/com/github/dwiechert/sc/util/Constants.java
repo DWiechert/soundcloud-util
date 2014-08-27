a892d0836db39783def38b5e5d1bc40d3c136ae9
Dan Wiechert <dan.wiechert@gmail.com>
2014-08-27 17:58:14 -0500

    Removed username and password.

--- src/main/java/com/github/dwiechert/sc/util/Constants.java
+++ src/main/java/com/github/dwiechert/sc/util/Constants.java
@@ -7,12 +7,6 @@ public final class Constants {
 	/** The SoundClound Client Secret. */
 	public static final String CLIENT_SECRET = "df5da35bce0e21efa7501f781d363308";
 
-	/** The SoundClound login username. */
-	public static final String SOUNDCLOUD_USERNAME = "mashpotatoemusic";
-
-	/** The SoundClound login password. */
-	public static final String SOUNDCLOUD_PASSWORD = "1535irving";
-
 	/** The URL for getting tracks from a SoundCloud song. Must format it with a song id and client id. */
 	public static final String TRACK_URL = "https://api.soundcloud.com/tracks/%s?client_id=%s";
 
--- src/main/java/com/github/dwiechert/sc/util/commands/Command.java
+++ src/main/java/com/github/dwiechert/sc/util/commands/Command.java
@@ -1,13 +1,10 @@
 package com.github.dwiechert.sc.util.commands;
 
-import java.io.IOException;
-
 import org.apache.commons.cli.Options;
 
 import com.github.dwiechert.sc.util.Constants;
 import com.soundcloud.api.ApiWrapper;
 import com.soundcloud.api.CloudAPI;
-import com.soundcloud.api.Token;
 
 public abstract class Command {
 	public abstract String getName();
@@ -18,27 +15,5 @@ public abstract class Command {
 
 	public abstract Options getOptions();
 
-	protected final CloudAPI api;
-
-	public Command() {
-		try {
-			this.api = cloudApi();
-		} catch (IOException e) {
-			throw new RuntimeException("Error initializing API.", e);
-		}
-	}
-
-	/**
-	 * Creates a {@link CloudAPI} bean.
-	 * 
-	 * @return A {@link CloudAPI} bean.
-	 * @throws IOException
-	 *             If an error occurs.
-	 */
-	private CloudAPI cloudApi() throws IOException {
-		final CloudAPI api = new ApiWrapper(Constants.CLIENT_ID, Constants.CLIENT_SECRET, null, null);
-		final Token token = api.login(Constants.SOUNDCLOUD_USERNAME, Constants.SOUNDCLOUD_PASSWORD);
-		api.setToken(token);
-		return api;
-	}
+	protected final CloudAPI api = new ApiWrapper(Constants.CLIENT_ID, Constants.CLIENT_SECRET, null, null);
 }
