5a69b01db10be480ae37d3966b53bfcc01a622ec
dwiechert <dan.wiechert@gmail.com>
2015-01-22 17:10:25 -0600

    Got basic mp3 tagging done.

--- src/main/java/com/github/dwiechert/mp3/Mp3Tagger.java
+++ src/main/java/com/github/dwiechert/mp3/Mp3Tagger.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 
 import com.github.dwiechert.sc.util.models.Mp3Metadata;
 import com.mpatric.mp3agic.ID3v1;
-import com.mpatric.mp3agic.ID3v2;
-import com.mpatric.mp3agic.ID3v24Tag;
+import com.mpatric.mp3agic.ID3v1Tag;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.NotSupportedException;
@@ -21,23 +20,23 @@ public class Mp3Tagger {
 			if (mp3File.hasId3v1Tag()) {
 				tag = mp3File.getId3v1Tag();
 			} else {
-				tag = new ID3v24Tag();
+				tag = new ID3v1Tag();
 				mp3File.setId3v1Tag(tag);
 			}
 
 			tag.setTitle(metadata.getTitle());
 			tag.setArtist(metadata.getArtist());
-			tag.setComposer(metadata.getComposer());
-			tag.setPublisher(metadata.getPublisher());
-			tag.setOriginalArtist(metadata.getOriginalArtist());
-			tag.setAlbumArtist(metadata.getAlbumArtist());
+//			tag.setComposer(metadata.getComposer());
+//			tag.setPublisher(metadata.getPublisher());
+//			tag.setOriginalArtist(metadata.getOriginalArtist());
+//			tag.setAlbumArtist(metadata.getAlbumArtist());
 			tag.setAlbum(metadata.getAlbum());
-			tag.setGenreDescription(metadata.getGenre());
+//			tag.setGenre(metadata.getGenre());
 			tag.setTrack(metadata.getTrackNumber());
-			tag.setUrl(metadata.getUrl());
-			tag.setCopyright(metadata.getCopyright());
+//			tag.setUrl(metadata.getUrl());
+//			tag.setCopyright(metadata.getCopyright());
 			tag.setComment(metadata.getComment());
-			tag.setEncoder(metadata.getEncoder());
+//			tag.setEncoder(metadata.getEncoder());
 			
 			mp3File.save(endFilename);
 		} catch (final UnsupportedTagException | InvalidDataException | IOException | NotSupportedException e) {
